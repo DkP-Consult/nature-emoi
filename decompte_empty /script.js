bcfdcc632a7d9b373fd9cb914539a928c2d97ac2
DkP <contact@dkp-consult.be>
2022-08-07 09:35:47 +0200

    Try deployement

---
+++ decompte_empty /script.js
@@ -0,0 +1,233 @@
+function myFunction () {
+  var day = new Date('2030-01-01');
+  var day1 = new Date('2022-06-23');
+  var day2 = new Date('2022-06-24');
+  var day3 = new Date('2022-06-25');
+  var day4 = new Date('2022-06-26');
+  var day5 = new Date('2022-06-27');
+  var day6 = new Date('2022-06-28');
+  var day7 = new Date('2022-06-29');
+  var day8 = new Date('2022-06-30');
+  var day9 = new Date('2022-07-01');
+  var day10 = new Date('2022-07-02');
+  var day11 = new Date('2022-07-03');
+  var day12 = new Date('2022-07-04');
+  var day13 = new Date('2022-07-05');
+  var day14 = new Date('2022-07-06');
+  var day14 = new Date('2022-07-07');
+  var day15 = new Date('2022-07-08')
+  var day16 = new Date('2022-07-09');
+  var day17 = new Date('2022-07-10');
+  var day18 = new Date('2022-07-11');
+  var day19 = new Date('2022-07-12');
+  var day20 = new Date('2022-07-13');
+  var day21 = new Date('2022-07-14');
+  var day22 = new Date('2022-07-15');
+  var day23 = new Date('2022-07-16');
+  var day24 = new Date('2022-07-17');
+  var day25 = new Date('2022-07-18');
+  var day26 = new Date('2022-07-19');
+  var day27 = new Date('2022-07-20');
+  var day28 = new Date('2022-07-21');
+  var day29 = new Date('2022-07-22');
+  var day30 = new Date('2022-07-23');
+  // Si day avant day1 && day avant day2 -> show //
+  if (day >= day1){
+    $("#c01").show()
+  }
+  else {
+    $("#c01").hide()
+  }
+
+  if (day >= day2){
+    $("#c02").show()
+}
+  else {
+    $("#c02").hide()
+}
+
+  if (day >= day3){
+    $("#c03").show()
+}
+  else {
+    $("#c03").hide()
+}
+
+  if (day >= day4){
+    $("#c04").show()
+}
+  else {
+    $("#c04").hide()
+}
+
+  if (day >= day5){
+    $("#c05").show()
+}
+  else {
+    $("#c05").hide()
+}
+  if (day >= day6){
+    $("#c06").show()
+}
+  else {
+    $("#c06").hide()
+}
+
+  if (day >= day7){
+    $("#c07").show()
+}
+  else {
+    $("#c07").hide()
+}
+
+  if (day >= day8){
+    $("#c08").show()
+}
+  else {
+    $("#c08").hide()
+}
+
+  if (day >= day9){
+    $("#c09").show()
+}
+  else {
+    $("#c09").hide()
+}
+
+  if (day >= day10){
+    $("#c10").show()
+}
+  else {
+    $("#c10").hide()
+}
+
+  if (day >= day11){
+    $("#c11").show()
+}
+  else {
+    $("#c11").hide()
+}
+  if (day >= day12){
+    $("#c12").show()
+}
+  else {
+    $("#c12").hide()
+}
+  if (day >= day13){
+    $("#c13").show()
+}
+  else {
+    $("#c13").hide()
+}
+  if (day >= day14){
+    $("#c14").show()
+}
+  else {
+    $("#c14").hide()
+}
+  if (day >= day15){
+    $("#c15").show()
+}
+  else {
+    $("#c15").hide()
+}
+  if (day >= day16){
+    $("#c16").show()
+}
+  else {
+    $("#c16").hide()
+}
+  if (day >= day17){
+    $("#c17").show()
+}
+  else {
+    $("#c17").hide()
+}
+  if (day >= day18){
+    $("#c18").show()
+}
+  else {
+    $("#c18").hide()
+}
+  if (day >= day19){
+    $("#c19").show()
+}
+  else {
+    $("#c19").hide()
+}
+
+  if (day >= day20){
+    $("#c20").show()
+}
+  else {
+    $("#c20").hide()
+}
+
+  if (day >= day21){
+    $("#c21").show()
+}
+  else {
+    $("#c21").hide()
+}
+
+  if (day >= day22){
+    $("#c22").show()
+}
+  else {
+    $("#c22").hide()
+}
+  if (day >= day23){
+    $("#c23").show()
+}
+  else {
+    $("#c23").hide()
+}
+
+  if (day >= day24){
+    $("#c24").show()
+}
+  else {
+    $("#c24").hide()
+}
+
+  if (day >= day25){
+    $("#c25").show()
+}
+  else {
+    $("#c25").hide()
+}
+  if (day >= day26){
+    $("#c26").show()
+}
+  else {
+    $("#c26").hide()
+}
+
+  if (day >= day27){
+    $("#c27").show()
+}
+  else {
+    $("#c27").hide()
+
+}
+  if (day >= day28){
+    $("#c28").show()
+}
+  else {
+    $("#c28").hide()
+}
+
+  if (day >= day29){
+    $("#c29").show()
+}
+  else {
+    $("#c29").hide()
+}
+
+  if (day >= day30){
+    $("#c30").show()
+}
+  else {
+    $("#c30").hide()
+}
+}
