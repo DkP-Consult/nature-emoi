c757556bc081d39b89af8ce5a89ed51be95c451e
dkp-consult <contact@dkp-consult.be>
2023-03-09 22:49:06 +0100

    Learn : JS - Counter : done with help ;
    #To do : add a timer for ten click and alert with click per minutes (just for fun)

---
+++ freeCodeCamp/js-beginners-projects/02-counter/scripts.js
@@ -0,0 +1,47 @@
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count --;
+        } else if (styles.contains("increase")) {
+            count ++;
+            console.log("Added");
+        } else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = "green";
+        } 
+        if (count < 0) {
+            value.style.color = "red";
+        } 
+        if (count === 0) {
+            value.style.color = "gray";
+        }
+
+        if (count === 25 || count === -25) {
+            alert ("Tu abuses des click !")
+            count = 0;
+        }
+
+        value.textContent = count;
+    });
+    });
+
+
+
+
+
+
+/* const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+const increase = document.getElementById("increase"); */
+
+/* Can I do the same things with every button assigned to a const ? */
---
+++ freeCodeCamp/js-beginners-projects/readmed.md
@@ -0,0 +1,4 @@
+freeCodeCamp article for projects : https://www.freecodecamp.org/news/javascript-projects-for-beginners/
+
+John Smilga project : https://www.vanillajavascriptprojects.com/
+
