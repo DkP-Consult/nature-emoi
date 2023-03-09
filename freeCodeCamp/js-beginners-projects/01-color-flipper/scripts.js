b66e6942ec0a70f6c42ffe77114d013f008ff372
dkp-consult <contact@dkp-consult.be>
2023-03-09 21:43:23 +0100

    Learn :  JS Fix : JS error - Color Flipper : bad targetting html id

--- freeCodeCamp/js-beginners-projects/01-color-flipper/scripts.js
+++ freeCodeCamp/js-beginners-projects/01-color-flipper/scripts.js
@@ -2,12 +2,12 @@ const colors = ['#3366ff', '#ff33cc', '#999966', '#009999', '#ccff66'];
 
 const btn = document.getElementById("btn");
 
-// const color = document.querySelector(".colorref");
+const color = document.getElementById("colorref");
 
 btn.addEventListener("click", function() {
     const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
-    colorref.textContent = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber() {
