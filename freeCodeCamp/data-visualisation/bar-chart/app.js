e6e09334993ab6a7d5c16beea861d9433c5e280f
dkp-consult <contact@dkp-consult.be>
2024-05-03 15:36:21 +0200

    leran : bar chart

--- freeCodeCamp/data-visualisation/bar-chart/app.js
+++ freeCodeCamp/data-visualisation/bar-chart/app.js
@@ -1,59 +1,70 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const svg = d3.select('svg');
-    const svgWidth = +svg.attr('width');
-    const svgHeight = +svg.attr('height');
-    const margin = {top: 20, right: 20, bottom: 30, left: 50};
-    const width = svgWidth - margin.left - margin.right;
-    const height = svgHeight - margin.top - margin.bottom;
+    const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
     
-    const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
+    // Préparer le parseur de date pour le format spécifique de vos dates
+    const parseDate = d3.timeParse("%Y-%m-%d");
 
-    d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-        .then(data => {
-            const x = d3.scaleBand()
-                        .rangeRound([0, width])
-                        .padding(0.1)
-                        .domain(data.data.map(d => d[0]));
+    fetch(url).then(response => response.json())
+    .then(data => {
+        // Transformer les chaînes de date en objets Date
+        const dataset = data.data.map(d => [parseDate(d[0]), d[1]]);
 
-            const y = d3.scaleLinear()
-                        .rangeRound([height, 0])
-                        .domain([0, d3.max(data.data, d => d[1])]);
+        const margin = { top: 30, right: 30, bottom: 30, left: 50 };
+        const width = 800 - margin.left - margin.right;
+        const height = 500 - margin.top - margin.bottom;
 
-            g.append('g')
-                .attr('class', 'axis axis--x')
-                .attr('id', 'x-axis')
-                .attr('transform', `translate(0,${height})`)
-                .call(d3.axisBottom(x));
+        const svg = d3.select("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom);
 
-            g.append('g')
-                .attr('class', 'axis axis--y')
-                .attr('id', 'y-axis')
-                .call(d3.axisLeft(y).ticks(10, 's'))
-              .append('text')
-                .attr('transform', 'rotate(-90)')
-                .attr('y', 6)
-                .attr('dy', '0.71em')
-                .attr('text-anchor', 'end')
-                .text('PIB');
+        const g = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-                g.selectAll('.bar')
-                .data(data.data)
-                .enter().append('rect')
-                .attr('class', 'bar')
-                .attr('x', d => x(d[0]))
-                .attr('y', d => {
-                    console.log('Y position for GDP value:', d[1], 'is', y(d[1])); // Débogage
-                    return y(d[1]);
-                })
-                .attr('width', x.bandwidth())
-                .attr('height', d => {
-                    let calculatedHeight = height - y(d[1]);
-                    console.log('Calculated height:', calculatedHeight, 'for GDP value:', d[1]); // Débogage
-                    return calculatedHeight;
-                })
-                .attr('data-date', d => d[0])
-                .attr('data-gdp', d => d[1]);
-            
+        // Utiliser une échelle de temps pour l'axe des x
+        const x = d3.scaleTime()
+            .range([0, width])
+            .domain(d3.extent(dataset, d => d[0]));
+
+        const y = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max(dataset, d => d[1])]);
+
+        g.append("g")
+            .attr("id", "x-axis")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x));
+
+        g.append("g")
+            .attr("id", "y-axis")
+            .call(d3.axisLeft(y));
+
+        // Modifier l'attribut 'width' pour gérer l'espacement des barres
+        const barWidth = width / dataset.length;
+
+        g.selectAll(".bar")
+            .data(dataset)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", d => x(d[0]))
+            .attr("y", d => y(d[1]))
+            .attr("width", barWidth)  // Utiliser barWidth pour un espacement uniforme
+            .attr("height", d => height - y(d[1]))
+            .attr("data-date", d => d3.timeFormat("%Y-%m-%d")(d[0]))  // Formatter les dates pour l'attribut data-date
+            .attr("data-gdp", d => d[1]);
+
+        const tooltip = d3.select("body").append("div")
+            .attr("id", "tooltip")
+            .style("opacity", 0);
+
+        g.selectAll(".bar")
+            .on("mouseover", function(event, d) {
+                tooltip.transition().duration(200).style("opacity", 0.9);
+                tooltip.html(d3.timeFormat("%Y-%m-%d")(d[0]) + '<br>' + '$' + d[1] + ' Billion')
+                    .attr("data-date", d3.timeFormat("%Y-%m-%d")(d[0]))
+                    .style("left", (event.pageX) + "px")
+                    .style("top", (event.pageY - 28) + "px");
+            })
+            .on("mouseout", function() {
+                tooltip.transition().duration(500).style("opacity", 0);
             });
-
+    });
 });
